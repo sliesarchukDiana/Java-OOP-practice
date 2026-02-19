af95b8395f445592ed218b751187aeaea08daa9e
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-19 14:52:23 +0200

    Add task_4 lab_1 Java

---
+++ lab-1/src/task_4.java
@@ -0,0 +1,26 @@
+void main() {
+    IO.println(countOddSum(0));
+    IO.println(countOddSum(5));
+    IO.println(countOddSum(-5));
+    IO.println(countOddSum(9));
+    IO.println(countOddSum(-10));
+}
+
+int countOddSum (int edge){
+    int sum = 0;
+    if (edge >= 0){       // if (edge > 0){
+        int i = 1;
+        while (i <= edge){
+            sum += i;
+            i+=2;
+        }
+    }
+    else{
+    int i = -1;
+    while (i >= edge){   // while (i >= edge){
+        sum+= i;
+        i-=2;
+    }
+    }
+    return sum;
+}
