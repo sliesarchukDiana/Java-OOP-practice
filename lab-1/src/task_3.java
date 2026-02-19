f8d7805d410f26d35cef8a4976ca1a69fd7069ee
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-19 13:58:04 +0200

    Add task_3 lab_1 Java

---
+++ lab-1/src/task_3.java
@@ -0,0 +1,25 @@
+String convertValToMonth(int val){
+    return switch (val) {
+        case 1 -> ("January");
+        case 2 -> ("February");
+        case 3 -> ("March");
+        case 4 -> ("April");
+        case 5 -> ("May");
+        case 6 -> ("June");
+        case 7 -> ("July");
+        case 8 -> ("August");
+        case 9 -> ("September");
+        case 10 -> ("October");
+        case 11 -> ("November");
+        case 12 -> ("December");
+        default -> ("Not a month.");
+    };
+}
+
+void main() {
+    IO.println(convertValToMonth(1));
+    IO.println(convertValToMonth(2));
+    IO.println(convertValToMonth(5));
+    IO.println(convertValToMonth(9));
+    IO.println(convertValToMonth(32));
+}
