61e8db06f9e01d05c94d26efa5a28738ef53cf87
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-25 11:19:47 +0200

    Format commentaries (while edge inclusion) task_4 lab_1

--- lab-1/src/task_4.java
+++ lab-1/src/task_4.java
@@ -1,15 +1,15 @@
 int countOddSum (int edge){
     int sum = 0;
-    if (edge >= 0){       // if (edge > 0){
+    if (edge >= 0){
         int i = 1;
-        while (i <= edge){
+        while (i <= edge){ // while (i < edge){
             sum += i;
             i+=2;
         }
     }
     else{
     int i = -1;
-    while (i >= edge){   // while (i >= edge){
+    while (i >= edge){   // while (i > edge){
         sum+= i;
         i-=2;
     }
