c873c14d0f8860b8f83730c8c0db28b172ceb28c
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-02-28 21:56:47 +0200

    Add exception handling (invalid URL format) task_1 lab-2

--- lab-2/src/task_1.java
+++ lab-2/src/task_1.java
@@ -1,6 +1,24 @@
-void main() {
-    IO.println("Hello and welcome!");
-    for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
+import java.net.URI;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+boolean checkURLValidity (String stringURL){
+    if (stringURL == null || stringURL.isEmpty()){
+        return false;
     }
+    try {
+        URL url = new URI(stringURL).toURL();
+        return true;
+    }
+    catch (MalformedURLException | URISyntaxException e) {
+        System.out.println("Incorrect URL format: " + e.getMessage());
+        return false;
+    }
+}
+
+void main() {
+    System.out.println(checkURLValidity("https://github.com"));
+    System.out.println(checkURLValidity("hop://github.com"));
+    System.out.println(checkURLValidity("http://guthib.com"));
 }
--- lab-2/src/task_2.java
+++ lab-2/src/task_2.java
@@ -1,3 +1,6 @@
 void main() {
-    
+    IO.println("Hello and welcome!");
+    for (int i = 1; i <= 5; i++) {
+        IO.println("i = " + i);
+    }
 }
