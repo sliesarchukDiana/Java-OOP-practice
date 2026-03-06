8bccbcdc2279cc23f126ef42db9a5d40ef2c9c9e
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-03-06 22:24:07 +0200

    Override equals and hashCode functions for Student class (For adequate object comparison)

--- lab-3/src/main/java/org/student_reg/Student.java
+++ lab-3/src/main/java/org/student_reg/Student.java
@@ -1,4 +1,5 @@
 package org.student_reg;
+import java.util.Objects;
 
 public class Student {
     String firstName, lastName;
@@ -15,6 +16,20 @@ public class Student {
     @Override
     public String toString() {
         return firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o){
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         Student student = (Student) o;
+         return age == student.age && Float.compare(student.averageGrade, averageGrade) == 0 &&
+        Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode (){
+         return Objects.hash(firstName, lastName, age, averageGrade);
     }
 }
 
