5f4e00f2c724d8fd72385d166aadf168fc3874d9
sliesarchukDiana <slesarcukdiana8@gmail.com>
2026-03-28 23:33:10 +0200

    Add Main as a test of CRUD operations in DB

--- lab-5/src/main/java/org/example/Main.java
+++ lab-5/src/main/java/org/example/Main.java
@@ -1,14 +1,51 @@
 package org.example;
-
-import dao.PersonDao;
+import dao.*;
+import entity.Keyword;
+import java.util.List;
 
 public class Main {
     static void main() {
-        System.out.println("Let's get started...");
-
+        System.out.println("Starting the work...\n");
         PersonDao personDao = new PersonDao();
-        personDao.searchPersonsWithMetadata("Олег");
+        MaterialDao materialDao = new MaterialDao();
+        KeywordDao keywordDao = new KeywordDao();
+        AuthorDao authorDao = new AuthorDao();
+        CatalogueSectionDao sectionDao = new CatalogueSectionDao();
+
+        System.out.println("\nTotal Users registered: " + personDao.getAllPersons().size());
+        System.out.println("Total Authors registered: " + authorDao.getAllAuthors().size());
+        System.out.println("Total Materials available: " + materialDao.getAllMaterials().size());
+        System.out.println("Total Catalogue Sections: " + sectionDao.getAllSections().size());
+        System.out.println("Total Keywords in system: " + keywordDao.getAllKeywords().size());
+        System.out.println("-------------------------------------------------------\n");
+
         personDao.searchPersonsWithMetadata("thekidslover");
-        personDao.searchPersonsWithMetadata("Абракадабра");
+        personDao.searchPersonsWithMetadata("Стівен");
+
+        System.out.println("\nCRUD");
+        System.out.println("CREATE: Adding a new tag...");
+        Keyword testKeyword = new Keyword();
+        testKeyword.setWord("Java для чайників");
+        keywordDao.createKeyword(testKeyword);
+        System.out.println("READ: Finding the tag...");
+        List<Keyword> allKeywords = keywordDao.getAllKeywords();
+        Keyword foundKeyword = null;
+        for (Keyword k : allKeywords) {
+            if (k.getWord().equals("Java для чайників")) {
+                foundKeyword = k;
+                System.out.println("Tag found in DB! Assigned ID: " + k.getIdKeyword());
+                break;
+            }
+        }
+        if (foundKeyword != null) {
+            System.out.println("3. UPDATE: Changing the tag name...");
+            foundKeyword.setWord("Java для початківців");
+            keywordDao.updateKeyword(foundKeyword);
+            System.out.println("Tag updated to: " + foundKeyword.getWord());
+
+            System.out.println("4. DELETE: Cleaning up the database...");
+//            keywordDao.deleteKeyword(foundKeyword.getIdKeyword());
+            System.out.println("Temporary tag deleted successfully.");
+        }
     }
 }
